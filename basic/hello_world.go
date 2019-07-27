e13a5ff7edfe29b77e31ff76aad4ebd4e0c11207
Johannes Lüdke <johannes.luedke@gmail.com>
2019-07-27 10:24:42 +0200

    semicolon, variable declaration

--- basic/hello_world.go
+++ basic/hello_world.go
@@ -7,4 +7,14 @@ func main() {
 	fmt.Println("Hello World")
 	// Jan 2 15:04:05 2006 MST
 	fmt.Println("Now is the " + time.Now().Format("02.01.2006 15:04"))
+	/*
+		This is a multi-line comment
+	*/
+	fmt.Println("statement separated by ';'")
+	fmt.Println("second")
+
+	var firstVar, secondVar int
+	firstVar = 5
+	secondVar = 8
+	fmt.Println(firstVar * secondVar)
 }
